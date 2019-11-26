136e682a5a6c1299a130769bfbccd25c7ccb6ee1
Guillermo MC <gmo.mc.10@gmail.com>
2019-11-25 22:39:41 -0600

    section 7 class 88 - creation lifecycle

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react';
 import styles from './Person.css';
 
 const person = ( props ) => {
-
+    console.log('[Person.js] rendering');
     return (
         <div className={styles.Person}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
